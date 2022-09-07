080cbb168a136a9be7756e15efeb53a8b060d8ba
jlewisit <joelewis.mortgage@gmail.com>
2022-09-07 07:10:13 -0700

    html template and index.js drafts complete

--- index.js
+++ index.js
@@ -2,81 +2,161 @@ const inquirer = require('inquirer')
 const path = require('path')
 const fs = require('fs')
 
-const Manager = require(".lib/Manager");
-const Engineer = requrie(".lib/Engineer");
-const Intern = require(".lib/Intern");
+const Manager = require("./lib/Manager");
+const Engineer = requrie("./lib/Engineer");
+const Intern = require("./lib/Intern");
 
-const employeeQuestions = require(".lib/Employee-questions");
-const managerQuestions = require(".lib/Manager-questions");
-const engineerQuestions = require(".lib/Engineer-questions");
-const internQuestions = require(".lib/Intern-questions");
+const employeeQuestions = require("./lib/Employee-questions");
+const managerQuestions = require("./lib/Manager-questions");
+const engineerQuestions = require("./lib/Engineer-questions");
+const internQuestions = require("./lib/Intern-questions");
 
-function appMenu() {
-  function createManager() {
-    console.log("Who is the manager?");
-    inquirer.prompt([
-      {
-      type: "input", 
-      name: "managerName",
-      message: "What is the manager's name?",
-      validate: (answer) => {
-        if (answer !== "") {
-          return true;
-        } 
-        else {
-          return "Please enter at least one character.";
-        }
-      },
-      {      
-      type: "input", 
-      name: "managerId",
-      message: "What is the manager ID?",
-      validate: (answer) => {
-        if (answer !== "") {
-          return true;
-        } 
-        else {
-          return "Please enter at least one character.";
-        }
-      },
-      {
-        type: "input",
-        name: "managerEmail",
-        message: "What is the manager's email?",
-        validate: (answer) => {
-          if (answer !== "") {
-            return true;
-          } 
-          else {
-            return "Please enter at least one character.";
-          }
-        },
-        {
-          type: "input",
-          name: "managerOfficeNumber",
-          message: "What is the manager's office number?",
-          validate: (answer) => {
-            if (answer !== "") {
-              return true;
-            } 
-            else {
-              return "Please enter at least one character.";
-            }
-          },
-      ])
-      .then((answers) => {
-        const manager = new Manager(
-          answers.managerName,
-          answers.managerID,
-          answers.managerEmail,
-          answers.managerOfficeNumber
-        );
+// Array of team members
+var teamMemberArray = [];
 
-      }
-  }
+// Write output HTML
+const writeToFile = (htmlHolderArray) => {
+  return new Promise((resolve, reject) => {
+    // Make output file name === team name
+    outputFileName = teamMemberArray[0];
+
+fs.writeFile( `./dist/${outputFileName}.html`, htmlHolderArray.join( "" ), function(err) {
+  // If there's an error, reject the Promise and send the error to the 
+  // Promise's `.catch() ` method.
+  if (err) {
+        reject(err);
+     // Return ouf of the function here to make sure the Promise doesn't
+     // accidentally execute the resolve() function as well.
+     return;
+    };
+
+  // If everything went well, resolve the Promise and send the succesful
+  // data to the `.then()` method.
+  resolve({
+        ok: true,
+        message: "All done! Your output HTML file is located in the 'dist' folder."
+    });
+});
+});
+};
+
+////////////////////////////////
+
+// Generate HTML
+function generateHtmlFile() {
+  const htmlHolderArray = htmlHolder.generateHtml(teamMemberArray);
+  writeToFile(htmlHolderArray);
 }
 
+// Add the intern profile to the teamProfileArr array.
+function addInternProfile() {
+  inquirer.prompt(internQuestions)
+  .then (function(data) {
+     const internName = data.internName;
+     const internId = data.internId;
+     const internEmail = data.internEmail;
+     const internSchool = data.internSchool;
+     const teamMember = new Intern(internName, internId, internEmail, internSchool);
 
+     teamProfileArr.push(teamMember);
 
-// Function call to start application
-appMenu();
+     // Populate the menu choices again to see what the user wants to do next.
+     addNonManagerTeamMember();
+  });
+};
+
+////////////////////////////////////////////
+////////////////////////////////////////////
+
+// Add the engineer profile to the teamProfileArr array.
+function addEngineerProfile() {
+  inquirer.prompt(engineerQuestions)
+  .then (function(data) {
+     const engineerName = data.engineerName;
+     const engineerId = data.engineerId;
+     const engineerEmail = data.engineerEmail;
+     const engineerGithubUsername = data.engineerGithubUsername;
+     const teamMember = new Engineer(engineerName, engineerId, engineerEmail, engineerGithubUsername);
+     
+     teamProfileArr.push(teamMember);
+
+     // Populate the menu choices again to see what the user wants to do next.
+     addNonManagerTeamMember();
+  });
+};
+
+////////////////////////////////////////////
+////////////////////////////////////////////
+
+// Populate the menu choices again to see what the user wants to do next.
+// Call the appropriate function based on the user's choice.
+function addNonManagerTeamMember() {
+  inquirer.prompt(addEmployeeQuestion)
+  .then(function(data) {
+     switch (data.menuChoices) {
+        case "Add an Engineer Profile":
+           addEngineerProfile();
+           break;
+        case "Add an Intern Profile":
+           addInternProfile();
+           break;
+        case "Done Generating Profiles":
+           generateHtmlFile();
+        break;
+     };
+  });
+};
+
+////////////////////////////////////////////
+////////////////////////////////////////////
+
+// Add the engineer profile to the teamProfileArr array.
+function addManagerProfile() {
+  inquirer.prompt(managerQuestions)
+  .then(function(data) {
+     const managerName = data.managerName;
+     const managerId = data.managerId;
+     const managerEmail = data.managerEmail;
+     const managerOfficeNum = data.managerOfficeNum;
+     const teamMember = new Manager(managerName, managerId, managerEmail, managerOfficeNum);
+     
+     teamProfileArr.push(teamMember);
+
+     // Populate the menu choices again to see what the user wants to do next.
+     addNonManagerTeamMember();
+  });
+};
+
+////////////////////////////////////////////
+////////////////////////////////////////////
+
+// Start the application by displaying a welcome message.
+// Then populate prompts to get the user input for the manager profile.
+function init() {
+  inquirer.prompt([
+     {
+        message: "Welcome!  Let's get started.  Please input your team name:",
+        name: "teamName",
+        validate: teamNameInput => {
+           if (teamNameInput && teamNameInput.trim().length > 0) {
+              return true;
+           }
+           else {
+              console.log( "Input your team name:" );
+              return false;
+           };
+        }
+     }
+  ])
+  .then(function(data) {
+     const teamName = data.teamName;
+     teamProfileArr.push(teamName);
+     addManagerProfile();
+  });
+};
+
+////////////////////////////////////////////
+////////////////////////////////////////////
+
+// Function call to initialize application.
+init();
